66911dcccf60883ee62b4e0dc3e3e4406ec8d7e3
Eduardo Garcia <egarcia@ooyala.com>
2015-05-16 18:13:40 -0500

    Move destination directories and files to paths

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,27 @@ var gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     paths = {
       js: ['app/main.js', 'app/react.jsx'],
-      css: ['app/main.scss']
+      jsMin: 'app.min.js',
+      css: ['app/main.scss'],
+      cssMin: 'app.min.css',
+      dist: 'dist'
     };
 
 gulp.task('scripts', function () {
   return gulp.src(paths.js)
     .pipe(react())
-    .pipe(concat('app.min.js'))
+    .pipe(concat(paths.jsMin))
     .pipe(uglify())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(paths.dist))
     .pipe(notify({ message: 'Finished transpiling jsx, and concatenating and minifying JavaScript'}));
 });
 
 gulp.task('styles', function () {
   return gulp.src(paths.css)
     .pipe(sass())
-    .pipe(concat('app.min.css'))
+    .pipe(concat(paths.cssMin))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(paths.dist))
     .pipe(notify({ message: 'Finished compiling SASS and minifying CSS'}));
 });
 
