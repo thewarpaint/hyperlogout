ade5e05fc7bb50642553a5062e6a0a4fbd798bbb
Eduardo Garcia <egarcia@ooyala.com>
2015-05-16 17:59:11 -0500

    Add gulp tasks for SASS compilation and CSS minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     notify = require('gulp-notify'),
     react = require('gulp-react'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css'),
     paths = {
-      js: ['app/main.js', 'app/react.jsx']
+      js: ['app/main.js', 'app/react.jsx'],
+      css: ['app/main.scss']
     };
 
 gulp.task('scripts', function () {
@@ -16,8 +19,18 @@ gulp.task('scripts', function () {
     .pipe(notify({ message: 'Finished transpiling jsx, and concatenating and minifying JavaScript'}));
 });
 
-gulp.task('watch', function(){
-  gulp.watch(paths.js, ['scripts']);
+gulp.task('styles', function () {
+  return gulp.src(paths.css)
+    .pipe(sass())
+    .pipe(concat('app.min.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({ message: 'Finished compiling SASS and minifying CSS'}));
 });
 
-gulp.task('build', ['scripts']);
+gulp.task('watch', function () {
+  gulp.watch(paths.js, ['scripts']);
+  gulp.watch(paths.css, ['styles']);
+});
+
+gulp.task('build', ['scripts', 'styles']);
